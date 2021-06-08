8e76a7a66cb665ed520a7755549d30d91a1f86e6
Lilach Daniel <lilachdaniel100@gmail.com>
2021-06-08 18:24:45 +0300

    changed implementation of lists

--- Data Structures - Project 2/src/DLList.java
+++ Data Structures - Project 2/src/DLList.java
@@ -1,6 +1,8 @@
-public class DLList<K> {
-    private DLNode<K> head;
-    private int size;
+import java.util.Iterator;
+
+public class DLList{
+    protected DLNode head;
+    protected int size;
 
     public DLList() {
         this.head = null;
@@ -9,20 +11,20 @@ public class DLList<K> {
 
     public int getSize() { return this.size; }
 
-    public void add(K val) {
+    public void add(Graph.Node node) {
         if (this.head == null) {
-            this.head = new DLNode<K>(val, null, null);
+            this.head = new DLNode(node, null, null);
         }
         else {
-            DLNode<K> newNode = new DLNode<K>(val, null, this.head);
-            this.head.prev = newNode;
+            DLNode newNode = new DLNode(node, null, this.head);
+            this.head.setPrev(newNode);
             this.head = newNode;
         }
 
         this.size++;
     }
 
-    public void delete(DLNode<K> toDelete) {
+    public void delete(DLNode toDelete) {
         // if deleting the head
         if (this.head == toDelete) {
             this.head = toDelete.getNext();
@@ -43,29 +45,94 @@ public class DLList<K> {
         this.size--;
     }
 
-    public DLNode<K> getFirst() {
+    public DLNode getFirst() {
         return this.head;
     }
 
-    public class DLNode<T> {
-        private T value;
-        private DLNode<T> prev;
-        private DLNode<T> next;
 
-        public DLNode(T val, DLNode<T> prev, DLNode<T> next) {
-            this.value = val;
+    public class DLNode {
+        private Graph.Node graphNode;
+        private DLNode prev;
+        private DLNode next;
+
+        public DLNode(Graph.Node graphNode, DLNode prev, DLNode next) {
+            this.graphNode = graphNode;
             this.prev = prev;
             this.next = next;
         }
 
-        public T getValue() { return this.value; }
-        public DLNode<T> getPrev() { return this.prev; }
-        public DLNode<T> getNext() { return this.next; }
+        public Graph.Node getGraphNode() { return this.graphNode; }
+        public DLNode getPrev() { return this.prev; }
+        public DLNode getNext() { return this.next; }
 
-        public void setPrev(DLNode<T> newPrev) { this.prev = newPrev; }
-        public void setNext(DLNode<T> newNext) { this.next = newNext; }
+        public void setPrev(DLNode newPrev) { this.prev = newPrev; }
+        public void setNext(DLNode newNext) { this.next = newNext; }
+
         public boolean hasNext() { return this.next != null; }
         public boolean hasPrev() { return this.prev != null; }
 
+    }
+}
+
+class NeighborsDLList extends DLList implements Iterable<NeighborsDLList.NeighborNode>{
+    @Override
+    public NeighborNode getFirst() {
+        return (NeighborNode) super.getFirst();
+    }
+
+    // DO NOT FORGET: this method leaves the new neighbor without a twin
+    public NeighborNode addNewNeighbor(Graph.Node node) {
+        if (this.head == null) {
+            this.head = new NeighborNode(node, null, null, null);
+        }
+        else {
+            NeighborNode newNode = new NeighborNode(node, null, (NeighborNode) this.head, null);
+            this.head.setPrev(newNode);
+            this.head = newNode;
+        }
+
+        this.size++;
+
+        return (NeighborNode) this.head;
+    }
+
+    @Override
+    public Iterator<NeighborNode> iterator() {
+        return new NeighborDLListIterator();
+    }
+
+    public class NeighborDLListIterator implements Iterator<NeighborNode> {
+        private NeighborNode curr = getFirst();
+        @Override
+        public boolean hasNext() {
+            return curr != null;
+        }
+
+        @Override
+        public NeighborNode next() {
+            NeighborNode res = curr;
+            curr = curr.getNext();
+            return res;
+        }
+    }
+
+
+    protected class NeighborNode extends DLNode{
+        private NeighborNode twin;
+
+        private NeighborNode(Graph.Node graphNode, NeighborNode prev, NeighborNode next, NeighborNode twin) {
+            super(graphNode, prev, next);
+            this.twin = twin;
+        }
+
+        public NeighborNode getNext() { return (NeighborNode) super.getNext(); }
+        public NeighborNode getPrev() { return (NeighborNode) super.getPrev(); }
+
+        public NeighborNode getTwin() {
+            return twin;
+        }
+        public void setTwin(NeighborNode twin) {
+            this.twin = twin;
+        }
     }
 }
