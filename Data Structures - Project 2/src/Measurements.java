9c9709457f38ba32a82b1e7ae5e1dc20c87397bb
ofirgaash <ofirgaash@mail.tau.ac.il>
2021-06-09 10:18:16 +0300

    made sure measurements are valid, "21" takes ~30 mins to run

--- Data Structures - Project 2/src/Measurements.java
+++ Data Structures - Project 2/src/Measurements.java
@@ -1,10 +1,12 @@
 import java.util.Random;
+import java.util.TreeSet;
 
 public class Measurements {
     public static void main(String[] args) {
         Random rand = new Random();
 
-        for (int i = 6; i < 21; i++) {
+        for (int i = 6; i <= 21; i++) {
+            long start = System.nanoTime();
 
             System.out.print(i + ":\t");
 
@@ -12,7 +14,11 @@ public class Measurements {
 
             Graph g = createGraph(numNodes);
 
-            System.out.println("highest rank is: " + (g.maxNeighborhoodWeight().getNeighborhoodWeight() - 1));
+            long end = System.nanoTime();
+
+            System.out.print("highest rank is: " + (g.maxNeighborhoodWeight().getNeighborhoodWeight() - 1));
+            System.out.println("\t\tseconds: " + (end - start) / (double)1000000000);
+
         }
 
         System.out.println("done");
@@ -35,6 +41,7 @@ public class Measurements {
 
         return g;
     }
+
 
     public static Pair[] getRandEdges(int edgeNum, int graphSize) {
         long range = (long)((long)graphSize * (long)(graphSize-1) / 2);
@@ -73,6 +80,10 @@ public class Measurements {
             int y = (int)(newChoice - (x-1) * (long)(x-2) / 2);
             edges[i] = new Pair(x-1,y-1);
         }
+
+        for (int i = 1; i < choices.length; i++)
+            if (choices[i] == choices[i-1])
+                System.out.println("DUPLICATES");
 
         return edges;
     }
